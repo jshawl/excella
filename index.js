8d4d849b9c4d585dad84c19e1bbd9c5f7403e709
Jesse Shawl <jesse@jshawl.com>
2016-01-30 09:41:37 -0500

    add anagram solution

--- index.js
+++ index.js
@@ -11,4 +11,27 @@ app.post("/hello", function(req, res){
     response.push("Hello World");
   }
   res.json(response);
+});
+
+app.post("/anagram", function(req, res){
+  var annies = req.body
+  var response = [];
+  annies.forEach(function(anny){
+    response.push(isAnagram(anny[0],anny[1]))
+  })
+  res.json(response);
 })
+
+function isAnagram(one, two){
+  var firstLetters = one.split("");
+  var secondLetters = two.split("");
+  if(one.length != two.length) return false;
+  var bool = true;
+  firstLetters.forEach(function(l){
+    if(!two.match(l)) bool = false;
+  })
+  secondLetters.forEach(function(l){
+    if(!one.match(l)) bool = false;
+  })
+  return bool;
+}
