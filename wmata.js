2d427fdfefda95d3c1b67fcba7be75f94487d05e
Jesse Shawl <jesse@jshawl.com>
2016-01-30 14:50:04 -0500

    god damn

--- wmata.js
+++ wmata.js
@@ -4,15 +4,14 @@ var env = fs.existsSync("./env.js") ? require("./env.js") : process.env;
 var request = require("request");
 
 function getClosestStation(lat, lon, radius, callback){
-  request("https://api.wmata.com/Rail.svc/json/jStations?api_key=" + env.primary, function(err,res,body){
-    var stations = JSON.parse(body).Stations
-    stations.forEach(function(station){
-      var diff = getDistanceFromLatLonInM(station.Lat,station.Lon, lat, lon)
-      if(diff < radius){
-        callback(station)
-      }
+  var url = "https://api.wmata.com/Rail.svc/json/jStationEntrances?api_key=" + env.primary + "&Lat="+lat+"&Lon="+lon+"&Radius=" + radius;
+  console.log(url)
+  request(url , function(err,res,body){
+    var code = JSON.parse(body).Entrances[0].StationCode1;
+    var url = "https://api.wmata.com/Rail.svc/json/jStationInfo?api_key="+env.primary+"&StationCode=" + code;
+    request(url, function(err, res, body){
+      callback(JSON.parse(body))
     })
-   callback()
   })
 }
 
@@ -22,38 +21,14 @@ function getDepartures( code, callback ){
   })
 }
 
-// http://stackoverflow.com/a/27943/850825
-function getDistanceFromLatLonInM(lat1,lon1,lat2,lon2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
-  var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
-  return d * 1000;
-}
-
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
-}
-
-
 module.exports = function(req, res, next){
   console.log("GOT", req.body)
   getClosestStation(req.body.latitude, req.body.longitude, req.body.radius, function(station){
-     if(station){
+        console.log("ST", station)
         res.json({
           station: station.Name,
           stationLat: station.Lat,
           stationLon: station.Lon
         })
-      } else {
-        res.json()
-      }
   })
-
 }
